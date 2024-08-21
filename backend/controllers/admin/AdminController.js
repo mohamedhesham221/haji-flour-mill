98a9b00143810ee7976e1f196f954a3593631b99
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-08-21 10:36:30 +0530

    move generateUsername function in services directory.

--- backend/controllers/admin/AdminController.js
+++ backend/controllers/admin/AdminController.js
@@ -1,31 +1,6 @@
 import User from "../../models/User.js";
 import jwt from "jsonwebtoken";
-
-// Function to generate a random numeric string
-const generateRandomNumericString = (length) => {
-  return Math.floor(Math.random() * Math.pow(10, length)).toString().padStart(length, '0');
-}
-
-// Function to generate a unique username
-const generateUsername = async (firstName, existingUsernames) => {
-  let username;
-  let isUnique = false;
-  
-  while (!isUnique) {
-      // Generate a random numeric string of 4 digits
-      const randomNumericString = generateRandomNumericString(4);
-
-      // Create the username by concatenating the first name and the random numeric string
-      username = `${firstName.toLowerCase()}${randomNumericString}`;
-
-      // Check if the username is unique
-      if (!existingUsernames.some((el) => el.username === username)) {
-          isUnique = true;
-      }
-  }
-
-  return username;
-}
+import { generateUsername } from "../../services/generate-username.js";
 
 export const signupAdmin = async (userDetails) => {
   const { first_name, last_name, email, mobile, password } = userDetails;
---
+++ backend/services/generate-username.js
@@ -0,0 +1,25 @@
+// Function to generate a random numeric string
+const generateRandomNumericString = (length) => {
+  return Math.floor(Math.random() * Math.pow(10, length)).toString().padStart(length, '0');
+}
+
+// Function to generate a unique username
+export const generateUsername = async (firstName, existingUsernames) => {
+  let username;
+  let isUnique = false;
+  
+  while (!isUnique) {
+      // Generate a random numeric string of 4 digits
+      const randomNumericString = generateRandomNumericString(4);
+
+      // Create the username by concatenating the first name and the random numeric string
+      username = `${firstName.toLowerCase()}${randomNumericString}`;
+
+      // Check if the username is unique
+      if (!existingUsernames.some((el) => el.username === username)) {
+          isUnique = true;
+      }
+  }
+
+  return username;
+}
