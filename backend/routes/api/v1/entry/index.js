aa7d24d690034f430cbd497d8b88ee714b3d29ad
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-08-26 17:59:13 +0530

    Add authentication to getUserEntries route.

--- backend/routes/api/v1/entry/index.js
+++ backend/routes/api/v1/entry/index.js
@@ -3,6 +3,7 @@ import { authanticateAdmin } from "../../../../middlewares/authAdmin.js";
 import addNewEntry from "./add-new-entry.js";
 import getAllEntries from "./get-all-entries.js";
 import getUserEntries from "./get-user-entries.js";
+import passport from "passport";
 
 const router = Router();
 
@@ -10,7 +11,7 @@ router.post("/new", authanticateAdmin, addNewEntry);
 
 router.get("/all", authanticateAdmin, getAllEntries);
 
-router.get("/user/:username", getUserEntries);
+router.get("/user/:username", passport.authenticate("jwt", {session: false}), getUserEntries);
 
 
 export default router;
